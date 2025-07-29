4b9fea24bfd7611fe1be5838d1956af296f087e0
Julien Pillaud <julien.pillaud@gmail.com>
2025-07-30 00:34:36 +0200

    feat: enhance FastAPI error handling with detailed logging and location extraction

--- cleanstack/fastapi/exceptions.py
+++ cleanstack/fastapi/exceptions.py
@@ -1,3 +1,4 @@
+import traceback
 from typing import ClassVar
 
 from fastapi import FastAPI
@@ -33,16 +34,35 @@ class ExceptionRegistry:
 
 def add_exception_handler(app: FastAPI) -> None:
     @app.exception_handler(DomainError)
-    async def app_exception_handler(request: Request, error: DomainError) -> Response:
-        status_code = ExceptionRegistry.get_status_code(error)
+    async def app_exception_handler(request: Request, exc: DomainError) -> Response:
+        status_code = ExceptionRegistry.get_status_code(exc)
         if status_code:
             return JSONResponse(
                 status_code=status_code,
-                content={"detail": str(error)},
+                content={"detail": str(exc)},
             )
 
-        logger.error("Unhandled DomainError", exc_info=True)
+        logger.error(f"Unhandled DomainError\n{unhandled_error_log(exc)}")
         return PlainTextResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             content="Internal Server Error",
         )
+
+
+def unhandled_error_log(exc: BaseException) -> str:
+    location = extract_exception_location(exc)
+    message = "Consider to register with 'ExceptionRegistry.register'"
+    return f"{location}\n    raise {exc!r}\n{message}"
+
+
+def extract_exception_location(exc: BaseException) -> str:
+    tb = exc.__traceback__
+    if not tb:
+        return "Unknown location"
+
+    while tb.tb_next:
+        tb = tb.tb_next
+
+    summary = traceback.extract_tb(tb)
+    frame = summary[-1]
+    return f'File "{frame.filename}", line {frame.lineno}, in {frame.name}'
