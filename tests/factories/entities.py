3a26b2ffad76a409ce46b04d94b93f4ec390da2a
Julien Pillaud <84393176+julienpillaud@users.noreply.github.com>
2025-10-27 21:41:58 +0100

    fix: update dependencies (#20)

--- tests/factories/entities.py
+++ tests/factories/entities.py
@@ -17,9 +17,11 @@ class User(DomainModel):
     posts: list[Post]
 
 
-class PostEntityFactory(ModelFactory[Post]): ...
+class PostEntityFactory(ModelFactory[Post]):
+    __check_model__ = True
 
 
 class UserEntityFactory(ModelFactory[User]):
+    __check_model__ = True
     # create user without posts to avoid circular dependency
     posts: ClassVar[list[Post]] = []
