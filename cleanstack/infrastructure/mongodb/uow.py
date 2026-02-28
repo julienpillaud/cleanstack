2c9ed479c6062f49b425b931b3fa5a5b88c8c962
Julien Pillaud <84393176+julienpillaud@users.noreply.github.com>
2026-02-28 10:50:53 +0100

    feat: add unit of work pattern (#36)

---
+++ cleanstack/infrastructure/mongodb/uow.py
@@ -0,0 +1,58 @@
+from collections.abc import Iterator
+from contextlib import contextmanager
+
+from pydantic import BaseModel, ConfigDict
+from pymongo import MongoClient
+from pymongo.client_session import ClientSession
+from pymongo.database import Database
+
+from cleanstack.infrastructure.mongodb.logger import logger
+from cleanstack.infrastructure.mongodb.types import MongoDocument
+from cleanstack.uow import UnitOfWorkProtocol
+
+
+class MongoDBContext(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    client: MongoClient[MongoDocument]
+    database: Database[MongoDocument]
+
+    @classmethod
+    def from_settings(cls, host: str, database_name: str) -> MongoDBContext:
+        logger.debug("Creating MongoDB client")
+        client: MongoClient[MongoDocument] = MongoClient(
+            host=host,
+            uuidRepresentation="standard",
+        )
+        database = client[database_name]
+        return cls(client=client, database=database)
+
+
+class MongoDBUnitOfWork(UnitOfWorkProtocol):
+    def __init__(self, context: MongoDBContext) -> None:
+        self._session: ClientSession | None = None
+        self.client = context.client
+
+    @property
+    def session(self) -> ClientSession:
+        if self._session is None:
+            raise RuntimeError("No active session")
+        return self._session
+
+    @contextmanager
+    def transaction(self) -> Iterator[None]:
+        logger.debug("Starting MongoDB session")
+        self._session = self.client.start_session()
+        self._session.start_transaction()
+        try:
+            yield
+        finally:
+            logger.debug("Closing MongoDB session")
+            self._session.end_session()
+            self._session = None
+
+    def commit(self) -> None:
+        self.session.commit_transaction()
+
+    def rollback(self) -> None:
+        self.session.abort_transaction()
